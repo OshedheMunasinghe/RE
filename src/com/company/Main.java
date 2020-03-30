682bb60fc4d7429cc1c131e6db83a897da37079f
OshedheMunasinghe <oshedhe91@gmail.com>
2020-03-30 09:41:01 +0200

    added regual expression.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,26 @@
 package com.company;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the zipcode: ");
         String zipCode = scanner.nextLine();
-        System.out.println("Test: " + zipCode);
+        if (zipCode.matches("\\d{5}(-\\d{4})?$")) {
+            System.out.println(zipCode + " is a valid code.");
+        } else {
+            System.out.println(zipCode + " is not a valid code.");
+        }
+        String script = "Procratination is surely not the destination, should we talk about shiny things?";
+        Pattern pattern = Pattern.compile("(\\w*(sh|ti|su)\\w*)");
+        Matcher matcher = pattern.matcher(script);
+        while (matcher.find()){
+            System.out.println(matcher.group(1) + " is a shushy word because of " + matcher.group(2));
+        }//end while
 
-    }
+    }//end main
 }
