be36017e59daa77b71092e7a80f09af320cdbc60
OshedheMunasinghe <oshedhe91@gmail.com>
2020-03-30 08:32:25 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the zipcode: ");
+        String zipCode = scanner.nextLine();
+        System.out.println("Test: " + zipCode);
+
+    }
+}
